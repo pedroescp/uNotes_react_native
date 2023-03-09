55adbd41b14ff8d0ad817e39a5418a54bd076160
pedro <pedrohhss14@gmail.com>
2023-03-08 23:08:59 -0300

    Feat: make a navbar with create a notes with this open modal

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   StyleSheet,
   View,
   TouchableOpacity,
   Text,
   DrawerLayoutAndroid,
+  Modal,
+  TextInput,
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
@@ -12,6 +14,8 @@ export default function Navbar() {
   const onPress = () => {
     console.log("Botão de adicionar pressionado");
   };
+
+  const [modalVisible, setModalVisible] = useState(false);
 
   return (
     <DrawerLayoutAndroid
@@ -24,6 +28,43 @@ export default function Navbar() {
       )}
     >
       <View style={styles.container}>
+        <Modal
+          animationType="slide"
+          transparent={true}
+          style={styles.modalBody}
+          visible={modalVisible}
+          onRequestClose={() => {
+            //Alert.alert("Modal has been closed.");
+            setModalVisible(!modalVisible);
+          }}
+        >
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <TextInput
+                placeholder={"Titulo da nota"}
+                placeholderTextColor={"#9ea1a6"}
+                style={styles.inputTitle}
+              />
+
+              <TextInput
+                placeholder={"Escreva uma nota"}
+                multiline={true}
+                numberOfLines={4}
+                placeholderTextColor={"#9ea1a6"}
+                style={styles.inputBody}
+              />
+              <View style={styles.viewBar}>
+                <TouchableOpacity>
+                  <Ionicons name="ios-trash" size={28} color="#8e8e8e" />
+                </TouchableOpacity>
+                <TouchableOpacity>
+                  <Ionicons name="ios-archive" size={28} color="#8e8e8e" />
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+        </Modal>
+
         <View style={styles.navbar}>
           <TouchableOpacity style={styles.icon}>
             <Ionicons name="ios-menu-outline" size={28} color="white" />
@@ -40,7 +81,7 @@ export default function Navbar() {
         </View>
         <TouchableOpacity
           style={styles.button}
-          onPress={onPress}
+          onPress={() => setModalVisible(true)}
           activeOpacity={0.7}
         >
           <Ionicons name="ios-add" size={40} color="#303030" />
@@ -52,6 +93,16 @@ export default function Navbar() {
 }
 
 const styles = StyleSheet.create({
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+
   container: {
     flex: 1,
     backgroundColor: "#334155",
@@ -105,5 +156,77 @@ const styles = StyleSheet.create({
     backgroundColor: "#f5f5f5",
     alignItems: "center",
     justifyContent: "center",
+  },
+
+  inputTitle: {
+    width: "100%",
+    height: 44,
+    padding: 0,
+    marginTop: 5,
+    marginBottom: 10,
+    fontSize: 30,
+    fontWeight: "500",
+    color: '#b6c4dd',
+  },
+
+  inputBody: {
+    textAlignVertical: "top",
+    width: "100%",
+    height: "100%",
+    marginTop: 20,
+    marginBottom: 10,
+    fontSize: 30,
+    fontWeight: "900",
+    color: "#f6f7f8",
+  },
+
+  viewBar: {
+    backgroundColor: "#f3f3f3",
+    position: "absolute",
+    bottom: 10,
+    padding: 0,
+    borderRadius: 10,
+    height: 50,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+    gap: 50,
+    width: "100%",
+  },
+
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
+  },
+
+  modalView: {
+    margin: 20,
+    backgroundColor: "#334255",
+    borderRadius: 20,
+    padding: 20,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 10,
+      height: 10,
+    },
+    shadowOpacity: 1.25,
+    shadowRadius: 20,
+    elevation: 90,
+    width: "100%",
+    height: "100%",
+  },
+
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+
+  modalBody: {
+    backgroundColor: "#334155",
+    elevation: 5,
   },
 });
