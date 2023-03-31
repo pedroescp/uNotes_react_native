cfcf3efe7ac79241b791cfc8f013fca9eb86a5db
pedro <pedrohhss14@gmail.com>
2023-03-31 19:28:46 -0300

    Feat: create a part use axios to connect to server

--- src/utils/axios.tsx
+++ src/utils/axios.tsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+//IPV4 of server you use or of your machine
 const instance = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "http://10.10.118.153:10010/",
 });
 
 export default instance;
