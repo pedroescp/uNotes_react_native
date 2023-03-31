af4702f76448c64c104371752c0c4f2b93411ba2
pedro <pedrohhss14@gmail.com>
2023-03-31 19:30:24 -0300

    Feat: create a part to storage the register of user

---
+++ src/utils/asyncStorage.tsx
@@ -0,0 +1,32 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+// Salva um valor com uma chave
+export const saveData = async (key: any, value: any) => {
+  try {
+    await AsyncStorage.setItem(key, value);
+    console.log(`Dados salvos com sucesso para a chave ${key}`);
+  } catch (error) {
+    console.log(`Erro ao salvar dados para a chave ${key}: ${error}`);
+  }
+};
+
+// Obtém um valor de uma chave
+export const getData = async (key: any) => {
+  try {
+    const value = await AsyncStorage.getItem(key);
+    if (value !== null) {
+      return value;
+    }
+  } catch (error) {
+    console.log(`Erro ao recuperar dados para a chave ${key}: ${error}`);
+  }
+};
+
+export const removeData = async (key: any) => {
+  try {
+    await AsyncStorage.removeItem(key);
+    console.log(`Item removido com sucesso para a chave ${key}`);
+  } catch (error) {
+    console.log(`Erro ao remover item para a chave ${key}: ${error}`);
+  }
+};
