b386690ef0ed253bc0fbe9b2dc692349728a84a3
pedro <pedrohhss14@gmail.com>
2023-03-31 19:30:52 -0300

    Feat: start use a simple auth to user navegate

--- App.tsx
+++ App.tsx
@@ -1,85 +1,71 @@
+// import AppContent from "./AppContent";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { StyleSheet, View } from "react-native";
-import { Text } from "react-native-elements";
 import Home from "./src/components/home";
 import Login from "./src/components/login";
 import Navbar from "./src/components/navbar";
-import { StatusBar } from 'react-native';
-
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
+import NoteCharge from "./src/components/note";
+import { AuthProvider } from "./src/content/auth";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Login"
-          component={Login}
-          options={{
-            headerShown: false,
-            title: "Login",
-            headerStyle: {
-              backgroundColor: "#334155",
-            },
-            headerTintColor: "#fff",
-            headerTitleStyle: {
-              fontWeight: "bold",
-            },
-            headerTitleAlign: "center",
-          }}
-        />
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{
-            headerShown: false,
-            title: "Home",
-            headerStyle: {
-              backgroundColor: "#334155",
-            },
-            headerTintColor: "#fff",
-            headerTitleStyle: {
-              fontWeight: "bold",
-            },
-            headerTitleAlign: "center",
-          }}
-        />
-        <Stack.Screen
-          name="Navbar"
-          component={Navbar}
-          options={{
-            headerShown: false,
-            title: "Home",
-            headerStyle: {
-              backgroundColor: "#334155",
-            },
-            headerTintColor: "#fff",
-            headerTitleStyle: {
-              fontWeight: "bold",
-            },
-            headerTitleAlign: "center",
-          }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+      <NavigationContainer>
+         <AuthProvider>
+        <Stack.Navigator>
+       
+          <Stack.Screen
+            name="Login"
+            component={Login}
+            options={{
+              headerShown: false,
+              title: "Login",
+              headerStyle: {
+                backgroundColor: "#334155",
+              },
+              headerTintColor: "#fff",
+              headerTitleStyle: {
+                fontWeight: "bold",
+              },
+              headerTitleAlign: "center",
+            }}
+          />
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{
+              headerShown: false,
+              title: "Home",
+              headerStyle: {
+                backgroundColor: "#334155",
+              },
+              headerTintColor: "#fff",
+              headerTitleStyle: {
+                fontWeight: "bold",
+              },
+              headerTitleAlign: "center",
+            }}
+          />
+          <Stack.Screen
+            name="Notes"
+            component={NoteCharge}
+            options={{
+              headerShown: false,
+              title: "Home",
+              headerStyle: {
+                backgroundColor: "#334155",
+              },
+              headerTintColor: "#fff",
+              headerTitleStyle: {
+                fontWeight: "bold",
+              },
+              headerTitleAlign: "center",
+            }}
+          />
+          
+        </Stack.Navigator>
+        </AuthProvider>
+      </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "#334155",
-    alignItems: "center",
-    justifyContent: "center",
-    color: "#f28c18",
-    padding: 10,
-  },
-});
