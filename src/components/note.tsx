892529ee51e50545595aec7c2d58c4b4f48c3d9e
pedro <pedrohhss14@gmail.com>
2023-03-31 19:34:13 -0300

    Feat: create a component of notes to manipulate this

--- src/components/note.tsx
+++ src/components/note.tsx
@@ -1,28 +1,154 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { Ionicons } from "@expo/vector-icons";
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Modal,
+  SafeAreaView,
+  StatusBar,
+} from "react-native";
+import QuillEditor, { QuillToolbar } from "react-native-cn-quill";
+import api from "../utils/api";
+import Navbar from "./navbar";
 
 export default function NoteCharge() {
+  const [loading, setLoading] = useState(true);
+  const [notes, setNotes] = useState([]);
+  const [showEmpty, setShowEmpty] = useState(false);
+  const [modalVisible, setModalVisible] = useState(false);
+  const _editor = React.createRef();
+
+  useEffect(() => {
+    async function getNotes() {
+      //make a filter
+      try {
+        const response = await api.notesGet();
+        setNotes(response.data);
+        if (!response.data || response.data.length <= 0) setShowEmpty(true);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    getNotes();
+  }, []);
+
   return (
-    <View style={styles.card}>
-      <View style={styles.header}>
-        <Text style={styles.title}>asdasdasdasdasd</Text>
+    <Navbar>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        style={styles.modalBody}
+        visible={modalVisible}
+        onRequestClose={async () => {
+          if (title || bodyNote) {
+            try {
+              const response = await api.notesPost({
+                titulo: title,
+                texto: bodyNote,
+                criadorId: null,
+                usuarioAtualizacaoId: null,
+                documentoId: null,
+              });
+
+              console.log(response);
+              setModalVisible(!modalVisible);
+            } catch (error) {
+              console.log(error);
+            }
+          }
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            {/*               <TextInput
+                placeholder={"Titulo"}
+                placeholderTextColor={"#9ea1a6"}
+                value={title}
+                onChangeText={setTitle}
+                style={styles.inputTitle}
+              />
+
+              <TextInput
+                placeholder={"Escreva uma nota"}
+                multiline={true}
+                numberOfLines={4}
+                value={bodyNote}
+                onChangeText={setbodyNote}
+                placeholderTextColor={"#9ea1a6"}
+                style={styles.inputBody}
+              /> */}
+            <SafeAreaView style={styles.root}>
+              <StatusBar style="auto" />
+              <QuillEditor
+                autoSize
+                style={styles.QuillEditor}
+                ref={_editor}
+                initialHtml="<h1>Quill Editor for react-native</h1>"
+              />
+              <QuillToolbar
+                editor={_editor}
+                style={styles.QuillToolbar}
+                options="full"
+                theme="light"
+              />
+            </SafeAreaView>
+
+            {/* <View style={styles.viewBar}>
+                <TouchableOpacity>
+                  <Ionicons name="ios-trash" size={28} color="#8e8e8e" />
+                </TouchableOpacity>
+                <TouchableOpacity>
+                  <Ionicons name="ios-archive" size={28} color="#8e8e8e" />
+                </TouchableOpacity>
+              </View> */}
+          </View>
+        </View>
+      </Modal>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => setModalVisible(true)}
+        activeOpacity={0.7}
+      >
+        <Ionicons name="ios-add" size={40} color="#303030" />
+      </TouchableOpacity>
+
+      <View style={styles.div}>
+        {!loading &&
+          notes.map((note: any) => (
+            <View style={styles.card} key={note.id} id={note.id}>
+              <View style={styles.header}>
+                <Text style={styles.title}>{note.titulo}</Text>
+              </View>
+              <View style={styles.body}>
+                <Text style={styles.content}>{note.texto}</Text>
+              </View>
+            </View>
+          ))}
       </View>
-      <View style={styles.body}>
-        <Text style={styles.content}>sdasdasdasdasd</Text>
-      </View>
-    </View>
+    </Navbar>
   );
-};
+}
 
 const styles = StyleSheet.create({
+  div: {
+    flexDirection: "row",
+    gap: 10,
+    alignItems: "center",
+    flexGrow: 1,
+  },
+
   card: {
-    position: 'relative',
-    display: 'flex',
-    flexDirection: 'column',
-    overflow: 'hidden',
+    position: "relative",
+    display: "flex",
+    flexDirection: "column",
+    overflow: "hidden",
     borderRadius: 12,
-    backgroundColor: 'rgba(255, 255, 255, 0.7)',
-    shadowColor: '#000',
+    backgroundColor: "rgba(255, 255, 255, 0.7)",
+    shadowColor: "#000",
     shadowOffset: {
       width: 0,
       height: 3,
@@ -32,24 +158,173 @@ const styles = StyleSheet.create({
     elevation: 4,
   },
   header: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
     padding: 16,
   },
   title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: '#303030',
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "#303030",
   },
   body: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
     padding: 16,
   },
+
+  button: {
+    position: "absolute",
+    bottom: 35,
+    right: 20,
+    width: 70,
+    height: 70,
+    borderRadius: 50,
+    backgroundColor: "#f28c18",
+    alignItems: "center",
+    justifyContent: "center",
+    elevation: 5,
+  },
+
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+
+  container: {
+    flex: 1,
+    backgroundColor: "#334155",
+    alignItems: "center",
+    justifyContent: "center",
+    color: "#f28c18",
+    padding: 10,
+  },
+  navbar: {
+    position: "absolute",
+    top: 70,
+    left: 40,
+    right: 40,
+    height: 60,
+    backgroundColor: "#262b36",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    borderRadius: 50,
+  },
+  icon: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    alignItems: "center",
+    justifyContent: "center",
+  },
   content: {
-    fontSize: 16,
-    color: '#303030',
+    top: 170,
+    maxWidth: "100%",
+    flexDirection: "row",
+  },
+
+  drawerContent: {
+    flex: 1,
+    backgroundColor: "#f5f5f5",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  inputTitle: {
+    width: "100%",
+    height: 44,
+    padding: 0,
+    marginTop: 5,
+    marginBottom: 10,
+    fontSize: 30,
+    fontWeight: "500",
+    color: "#b6c4dd",
+  },
+
+  inputBody: {
+    textAlignVertical: "top",
+    width: "100%",
+    height: "100%",
+    marginTop: 20,
+    marginBottom: 10,
+    fontSize: 30,
+    fontWeight: "900",
+    color: "#f6f7f8",
+  },
+
+  viewBar: {
+    backgroundColor: "#f3f3f3",
+    position: "absolute",
+    bottom: 10,
+    padding: 0,
+    borderRadius: 10,
+    height: 50,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+    gap: 50,
+    width: "100%",
+  },
+
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
+  },
+
+  modalView: {
+    margin: 20,
+    backgroundColor: "#334255",
+    borderRadius: 20,
+    padding: 20,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 10,
+      height: 10,
+    },
+    shadowOpacity: 1.25,
+    shadowRadius: 20,
+    elevation: 90,
+    width: "100%",
+    height: "100%",
+  },
+
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+
+  modalBody: {
+    backgroundColor: "#334155",
+    elevation: 5,
+  },
+
+  QuillEditor: {
+    width: 500,
+    padding: 0,
+    maxWidth: `100%`,
+    backgroundColor: "#262b36",
+  },
+
+  QuillToolbar: {
+    position: "absolute",
+    flex: 1,
+  },
+
+  root: {
+    flex: 1,
   },
 });
