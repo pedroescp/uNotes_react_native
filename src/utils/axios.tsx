49661e1047d909230dc3b0e09065b8c997e0f993
pedro <pedrohhss14@gmail.com>
2023-04-18 22:46:19 -0300

    Feat: new route creatte

--- src/utils/axios.tsx
+++ src/utils/axios.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 //IPV4 of server you use or of your machine
 const instance = axios.create({
-  baseURL: "http://10.10.118.153:10010/",
+  baseURL: "http://192.168.18.12:10010/",
 });
 
 export default instance;
