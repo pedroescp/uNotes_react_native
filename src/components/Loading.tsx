2f424bb80f358df784ae3d52afabbbd6d70211fd
pedro <pedrohhss14@gmail.com>
2023-06-20 11:33:26 -0300

    Feat: add a new part of loadind with blur

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -4,7 +4,7 @@ import { Text } from "react-native-elements";
 
 export default function Loading() {
   return (
-    <View style={{position: "absolute"}}>
+    <View style={{position: "absolute", zIndex: 999, width: "110%", height: '110%', backgroundColor: 'rgba(0, 0, 0, 0.5)', alignItems: "center", justifyContent: "center"}}>
       <Lottie source={require("../../src/icons/loading.json")} autoPlay loop style={{width: 200, height: 200}} />
     </View>
   );
