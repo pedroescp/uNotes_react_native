3cbaf332012150b57fd3c89eb0d81e68af34eb80
pedro <pedrohhss14@gmail.com>
2023-06-20 20:02:50 -0300

    Feat: remove tags dont use

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -10,6 +10,7 @@ import {
 import React, { useEffect, useState } from "react";
 import { getData, removeData } from "../utils/asyncStorage";
 import api from "../utils/api";
+import { useRoute } from "@react-navigation/native";
 
 export default function Home({ navigation }: any) {
   const [user, setUser] = useState("");
@@ -17,6 +18,7 @@ export default function Home({ navigation }: any) {
 
   useEffect(() => {
     getUser();
+
   }, [user]);
 
   const getUser = async () => {
